76fb14177e57f8b96d31a1b51c90007bcbf99d42
pglusenko <94999093+pglusenko@users.noreply.github.com>
2026-05-28 18:05:15 +0000

    3.2

--- script.js
+++ script.js
@@ -170,7 +170,7 @@ function decryptDoubleBase64(doubleBase64Text, key) {
 }
 
 // Лічильник знайомства (26 лютого 2026 року)
-const startDate = new Date(2026, 1, 26, 23, 04); 
+const startDate = new Date(2026, 1, 26, 23, 4); 
 function updateCounter() {
     const now = new Date();
     
